5d07a0c0334cf3cff2705c54ff573c37c1d35721
hadasprintz <hadaspr100@gmail.com>
2025-01-05 12:48:42 +0200

    dispose works for all tables

--- HeadcountAllocation/Migrations/20250105104649_InitialCreate.Designer.cs
+++ HeadcountAllocation/Migrations/20250105104649_InitialCreate.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HeadcountAllocation.Migrations
 {
     [DbContext(typeof(DBcontext))]
-    [Migration("20250102172410_InitialCreate")]
+    [Migration("20250105104649_InitialCreate")]
     partial class InitialCreate
     {
         /// <inheritdoc />
