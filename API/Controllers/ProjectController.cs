27f99d723804ea7777e29e8518029e6df71bea78
noamalu <noamalu@post.bgu.ac.il>
2025-01-23 11:44:03 +0200

    create returns id

--- API/Controllers/ProjectController.cs
+++ API/Controllers/ProjectController.cs
@@ -20,7 +20,7 @@ namespace API.Controllers
         }
 
         [HttpPost("Create")] 
-        public ActionResult<Response> Create([Required][FromBody]Project project)
+        public ActionResult<Response<int>> Create([Required][FromBody]Project project)
         {            
             return Ok(_headCountService.CreateProject(project.ProjectName, project.Description, project.Deadline, project.RequiredHours, new()));
         }
@@ -38,36 +38,36 @@ namespace API.Controllers
             }
         }
 
-        [HttpPost("{projectId}/Roles")]
-        public ActionResult<Response> AddRole([Required][FromRoute]int projectId, 
-            [Required][FromBody]Role role)
-        {
-            try
-            {
-                // _headCountService.AddRoleToProject(role.RoleName, projectId,
-                //     GetValueById<TimeZones>(role.TimeZone), 
-                //     role.ForeignLanguages, role.Skills, 
-                //     role.YearsExperience, role.JobPercentage);
-                return Ok("Role added to project successfully");
-            }
-            catch (Exception ex)
-            {
-                return NotFound(ex.Message);
-            }
-        }
+        // [HttpPost("{projectId}/Roles")]
+        // public ActionResult<Response> AddRole([Required][FromRoute]int projectId, 
+        //     [Required][FromBody]Role role)
+        // {
+        //     try
+        //     {
+        //         // _headCountService.AddRoleToProject(role.RoleName, projectId,
+        //         //     GetValueById<TimeZones>(role.TimeZone), 
+        //         //     role.ForeignLanguages, role.Skills, 
+        //         //     role.YearsExperience, role.JobPercentage);
+        //         return Ok("Role added to project successfully");
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         return NotFound(ex.Message);
+        //     }
+        // }
 
-        [HttpGet("{projectId}/Roles")]
-        public ActionResult<Response<Role>> GetRoles([Required][FromRoute]int projectId)
-        {
-            try
-            {
-                return Ok(_headCountService.GetAllRolesByProject(projectId));
-            }
-            catch (Exception ex)
-            {
-                return NotFound(ex.Message);
-            }
-        }    
+        // [HttpGet("{projectId}/Roles")]
+        // public ActionResult<Response<Role>> GetRoles([Required][FromRoute]int projectId)
+        // {
+        //     try
+        //     {
+        //         return Ok(_headCountService.GetAllRolesByProject(projectId));
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         return NotFound(ex.Message);
+        //     }
+        // }    
 
     }
 }
