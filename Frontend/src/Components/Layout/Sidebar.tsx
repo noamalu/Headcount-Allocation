30645b801ac8bebc993b01eb40b181ad1ef43021
GiliVeltz <gilive2711@gmail.com>
2025-04-07 12:28:17 +0300

    Headcount-Allocation/Frontend/src/Components /Layout/ Updates

--- Frontend/src/Components/Layout/Sidebar.tsx
+++ Frontend/src/Components/Layout/Sidebar.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import '../../Styles/Layout.css'
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <ul>
         <li className="active">Dashboard</li>
-        <li>Projects</li>
-        <li>Employees</li>
+        <li>  
+          <NavLink to="/projects" className={getActiveClass}>Projects</NavLink>
+        </li>
+        <li>  
+          <NavLink to="/employees" className={getActiveClass}>Employees</NavLink>
+        </li>
+        <li>Calendar</li>
+        <li>
+          <NavLink to="/tickets" className={getActiveClass}>Tickets</NavLink>
+        </li>
         <li>Notifications</li>
       </ul>
     </div>
   );
 };
 
-export default Sidebar;
+const getActiveClass = ({ isActive }: { isActive: boolean }) => 
+  isActive ? 'active' : '';
 
+export default Sidebar;
